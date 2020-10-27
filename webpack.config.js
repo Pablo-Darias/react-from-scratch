778be8a1422c572471b774cf09ebae2c4ac233bd
Pablo-Darias <pablo.darias@s4rb.com>
2020-10-27 13:03:28 +0000

    React from scratch

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index/tsx",
+  output: {
+    filename: "main.bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [{ test: /\.tsx?$/, use: "ts-loader", exclude: /node_modules/ }],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  devServer: {
+    port: 3000,
+  },
+};
